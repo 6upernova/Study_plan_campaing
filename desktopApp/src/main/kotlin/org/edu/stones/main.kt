ff53b622485d0e6668714226c3064dfafdaedb49
Santiago_Reta_Puccerelli <santiagoreta@yahoo.com>
2026-06-23 15:18:48 -0300

    Se abre en pantalla completa

--- desktopApp/src/main/kotlin/org/edu/stones/main.kt
+++ desktopApp/src/main/kotlin/org/edu/stones/main.kt
@@ -1,6 +1,8 @@
 package org.edu.stones
 
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import org.edu.stones.presentation.App
 
@@ -8,6 +10,7 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "study_plan_campaing",
+        state = WindowState(placement = WindowPlacement.Maximized),
     ) {
         App()
     }
