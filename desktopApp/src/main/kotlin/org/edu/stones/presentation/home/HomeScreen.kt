7e65c3ccbd6ca89e7b47a13bf0afcdf3172af5ca
Nahuelk001 <nahuelcontreras2@outlook.com>
2026-06-23 16:25:28 -0300

    Cargando Carrera

--- desktopApp/src/main/kotlin/org/edu/stones/presentation/home/HomeScreen.kt
+++ desktopApp/src/main/kotlin/org/edu/stones/presentation/home/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -66,7 +67,7 @@ fun HomeScreen(
         }
 
         if (uiState.isLoading) {
-            CircularProgressIndicator()
+            showText("Diagramando Carrera...")
         } else {
             val layoutData = uiState.graphLayoutData
 
@@ -207,5 +208,24 @@ fun SubjectDetailWindow(
                 )
             }
         }
+    }
+}
+
+@Composable
+private fun showText(
+    text: String,
+    modifier: Modifier = Modifier
+){
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        GraphBackground()
+
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyLarge,
+            fontSize = 24.sp
+        )
     }
 }
